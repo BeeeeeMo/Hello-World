ea7a7ee6097a742e1097d4c658c9230cde72d7a1
BeeeeeMo <40410207@go.hwh.edu.tw>
2020-12-08 01:48:41 +0800

    Update lingvist.js

--- script/lingvist.js
+++ script/lingvist.js
@@ -1,19 +1,21 @@
-// QX: ^https://payment-api\.lingvist\.com/1\.4/user/services.+
-
 var obj = JSON.parse($response.body);
 obj = {
 	"services": [{
 		"service": "unlimited",
 		"is_infinite": true,
 		"active_since_ts": "2020-12-07T16:35:48.738672Z",
+		"active_until_ts": "9999-11-16T00:00:00.738672Z",
 		"payment_provider": "apple-in-app",
 		"product_name": "unlimited-lifetime",
 		"title": "Lingvist \u7121\u9650\u7248\uff0c\u55ae\u5e74\u4ed8\u8cbb",
 		"subscription": {
 			"is_recurring": true,
-			"uuid": "00000000-0000-0000-0000-000000000000",
+			"uuid": 00000000-0000-0000-0000-000000000000,
 			"group_name": "unlimited",
 			"status": "trial",
+			"next_billing_ts": "9999-11-16T00:00:00Z",
+			"expiration_ts": "9999-11-16T00:00:00Z",
+			"free_trial_duration": "P14D",
 			"price": {
 				"amount": "2490.000000",
 				"currency": "TWD"

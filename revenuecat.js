6b848007fa1895a44cad424ac75e3449edbfefeb
BeeeeeMo <BeeeeeMo0987@gmail.com>
2020-12-10 03:34:40 +0800

    MOZE 3.0

---
+++ revenuecat.js
@@ -0,0 +1,18 @@
+let obj = JSON.parse($response.body);
+obj["subscriber"]["subscriptions"]= {
+    "MOZE_PRO_SUBSCRIPTION_MONTHLY_BASIC": {
+        "billing_issues_detected_at": null,
+        "expires_date": "2099-11-16T11:16:00Z",
+        "grace_period_expires_date": null,
+        "is_sandbox": false,
+        "original_purchase_date": "2020-11-16T00:00:00Z",
+        "period_type": "normal",
+        "purchase_date": "2020-11-16T00:00:00Z",
+        "store": "app_store",
+        "unsubscribe_detected_at": "2099-12-06T17:52:25Z"
+    }
+    };
+// obj["subscriber"]["entitlements"]= {
+
+//     };
+$done({body: JSON.stringify(obj)});

6829c4c95c0ac07372315bb0e8fbe5bb3783327e
BeeeeeMo <BeeeeeMo0987@gmail.com>
2020-12-11 02:21:40 +0800

    test

---
+++ script/moze.js
@@ -0,0 +1,58 @@
+// api.moze.app
+// ^https://api\.moze\.app/iap/transaction-record/
+
+body = JSON.stringify({
+	"success": true,
+	"data": [{
+		"id": "000000000000000",
+		"date": "2020-11-16T00:00:00.000Z",
+		"state": 3,
+		"productId": "MOZE_IAP_ICONS_COLLECTION_01",
+		"createdAt": "2020-11-16T00:00:00.000Z",
+		"updatedAt": "2020-11-16T00:00:00.000Z"
+	}, {
+		"id": "000000000000000",
+		"date": "2020-11-16T00:00:00.000Z",
+		"state": 3,
+		"productId": "MOZE_IAP_ICONS_COLLECTION_02",
+		"createdAt": "2020-11-16T00:00:00.000Z",
+		"updatedAt": "2020-11-16T00:00:00.000Z"
+	}, {
+		"id": "000000000000000",
+		"date": "2020-11-16T00:00:00.000Z",
+		"state": 3,
+		"productId": "MOZE_IAP_ICONS_COLLECTION_03",
+		"createdAt": "2020-11-16T00:00:00.000Z",
+		"updatedAt": "2020-11-16T00:00:00.000Z"
+	}, {
+		"id": "000000000000000",
+		"date": "2020-11-16T00:00:00.000Z",
+		"state": 3,
+		"productId": "MOZE_IAP_THEMES_MARBLE_01",
+		"createdAt": "2020-11-16T00:00:00.000Z",
+		"updatedAt": "2020-11-16T00:00:00.000Z"
+	}, {
+		"id": "000000000000000",
+		"date": "2020-11-16T00:00:00.000Z",
+		"state": 3,
+		"productId": "MOZE_IAP_THEMES_WATERCOLOR_02",
+		"createdAt": "2020-11-16T00:00:00.000Z",
+		"updatedAt": "2020-11-16T00:00:00.000Z"
+	}, {
+		"id": "000000000000000",
+		"date": "2020-11-16T00:00:00.000Z",
+		"state": 3,
+		"productId": "MOZE_IAP_THEMES_WATERCOLOR_01",
+		"createdAt": "2020-11-16T00:00:00.000Z",
+		"updatedAt": "2020-11-16T00:00:00.000Z"
+	}, {
+		"id": "000000000000000",
+		"date": "2020-11-16T00:00:00.000Z",
+		"state": 3,
+		"productId": "MOZE_IAP_THEMES_SPACE_01",
+		"createdAt": "2020-11-16T00:00:00.000Z",
+		"updatedAt": "2020-11-16T00:00:00.000Z"
+	}]
+});
+
+$done(body);

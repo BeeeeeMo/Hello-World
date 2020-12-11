2d9831e723103ddf275d58d5188a14e7b3a4d0a1
BeeeeeMo <BeeeeeMo0987@gmail.com>
2020-12-12 02:54:34 +0800

    naviking

---
+++ script/localking.js
@@ -0,0 +1,13 @@
+// 導航王3D 試用
+// ^https://naviking3d\.localking\.com\.tw/api/cht/Check_Service_3DLite
+// naviking3d.localking.com.tw
+
+body = JSON.stringify({
+	"output_data": [{
+		"result": "True,2020/12/11,9999/01/10"
+	}],
+	"output_code": 1,
+	"output_msg": ""
+});
+
+$done(body);

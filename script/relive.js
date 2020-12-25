3d0244877f50ebdbb070aa4bbcf0d140483d84bb
BeeeeeMo <BeeeeeMo0987@gmail.com>
2020-12-26 03:12:29 +0800

    Full Crack

--- script/relive.js
+++ script/relive.js
@@ -2,4 +2,12 @@ var body = $response.body;
 var obj = JSON.parse($response.body)
 
 obj['user']['premium'] = true;
+obj['user']['max_photos'] = 50;
+obj['user']['max_moments'] = 50;
+obj['user']['max_duration'] = 60;
+obj['user']['features']['import'] = "show";
+obj['user']['features']['edit'] = "show";
+obj['user']['features']['music'] = "show";
+obj['user']['features']['settings_premium_trigger'] = "hide";
+obj['user']['features']['memories_premium_trigger'] = "hide";
 $done(JSON.stringify(obj));

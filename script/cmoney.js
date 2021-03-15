7c6e0ed9fb0e8e0aae59c8d2d9358f41c1ca6047
BeeeeeMo <40410207@go.hwh.edu.tw>
2021-03-15 22:49:33 +0800

    Update cmoney.js

--- script/cmoney.js
+++ script/cmoney.js
@@ -4,5 +4,5 @@ var isVIP = obj["AuthType"]
 
 if (isVIP == 0) {
       $done({body:JSON.stringify({"ResponseCode":1,"ResponseMsg":"","AuthType":1,"AuthExpTime":"2099/11/16"})});
-}
+};
 $done(body);

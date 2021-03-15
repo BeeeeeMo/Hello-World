f3eec605c1dd2aef251ea92bd6357b32c0a275df
BeeeeeMo <40410207@go.hwh.edu.tw>
2021-03-15 23:18:04 +0800

    Update cmoney.js

--- script/cmoney.js
+++ script/cmoney.js
@@ -2,7 +2,8 @@ var body = $response.body;
 var obj = JSON.parse(body)
 
 if (obj["AuthType"] == 0) {
-      body =  JSON.stringify({"ResponseCode":1,"ResponseMsg":"","AuthType":1,"AuthExpTime":"2099/11/16"});
+    $notify("Cmoney‼️", "", "Get!")
+    body =  JSON.stringify({"ResponseCode":1,"ResponseMsg":"","AuthType":1,"AuthExpTime":"2099/11/16"});
 };
 
 $done(body);

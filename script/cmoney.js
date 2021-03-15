d5f1233c561005e71e02b119784de5babd887d81
BeeeeeMo <40410207@go.hwh.edu.tw>
2021-03-15 22:43:08 +0800

    Create cmoney.js

---
+++ script/cmoney.js
@@ -0,0 +1,8 @@
+var body = $response.body;
+var obj = JSON.parse($response.body)
+var isVIP = obj["AuthType"]
+
+if (isVIP == 0) {
+      $done({body:JSON.stringify({"ResponseCode":1,"ResponseMsg":"","AuthType":1,"AuthExpTime":"2099/11/16"})});
+}
+$done(body);

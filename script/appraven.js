10cbdf9a0ea5d99e5eb2ece10c807eb15ed5e779
BeeeeeMo <40410207@go.hwh.edu.tw>
2021-04-01 13:45:05 +0800

    Update appraven.js

--- script/appraven.js
+++ script/appraven.js
@@ -1,6 +1,5 @@
 var body = $response.body;
-var obj = JSON.parse(body)
+var obj = JSON.parse($response.body)
 
-obj["premium"] = true;
-
-$done((JSON.stringify(obj));
+obj['premium'] = true;
+$done(JSON.stringify(obj));

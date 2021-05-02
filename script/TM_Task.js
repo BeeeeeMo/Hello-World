f314de6bb628f111217411959de2d991640542f7
BeeeeeMo <BeeeeeMo0987@gmail.com>
2021-05-02 21:31:19 +0800

    Update TM_Task.js

--- script/TM_Task.js
+++ script/TM_Task.js
@@ -1,23 +1,24 @@
 const url = "https://navikingtm.autoking.com.tw/api/v3/GetPassV2";
 const data = {"sourceid":"3","did":"AC923D46-1996-AC11-16F1-860903CF76AA"};
+const headers = {'user-agent': 'NaviKingTM/1 CFNetwork/1220.1 Darwin/20.3.0'};
+
 const myRequest = {
   url: url,
   method: "POST",
-  body: JSON.stringify(data)
+  body: JSON.stringify(data),
+  headers: headers,
 };
 
 $task.fetch(myRequest).then(response => {
     if (response.statusCode == 200){
-//         $notify("導航王測速更新", "更新成功", response.body); // Success!
-      //         $prefs.setValueForKey(url, 'NK_MAP_URL');
+      // $notify("導航王測速更新", "更新成功", response.body); // Success!
+      // $prefs.setValueForKey(url, 'NK_MAP_URL');
       $notify("導航王測速更新", "更新成功", response.body); // Success!
       var obj = JSON.parse(response.body);
       console.log(obj['output_data']['accessToken']);
       $done();
-    } else {
-      $notify("???", "statusCode", response.statusCode); // Success!
-      $done();
     }
+    $notify("???", "statusCode", response.statusCode); // Success!
     $done();
 }, reason => {
     // reason.error

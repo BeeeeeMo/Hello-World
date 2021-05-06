db959a0e30b8e4563398a717c5c7f149fbfd4c31
BeeeeeMo <BeeeeeMo0987@gmail.com>
2021-05-06 23:34:29 +0800

    Update navking_update.js

--- script/navking_update.js
+++ script/navking_update.js
@@ -1,57 +1,78 @@
-const url = "https://navikingtm.autoking.com.tw/api/v3/GetPassV2";
-const data = {"sourceid":"3","did":"AC923D46-1996-AC11-16F1-860903CF76AA"};
-const headers = {
-  'user-agent': 'NaviKingTM/1 CFNetwork/1220.1 Darwin/20.3.0',
-  'content-type': 'application/json'};
+let today = new Date().toJSON().slice(0,10).replace(/-/g,'_');
 
+const url = "https://download.localking.com.tw/cdnfiles/NaviKingUpdate/SpeedCameras/daily_camera_" + today + "_04.dat";
 const myRequest = {
-  url: url,
-  method: "POST",
-  body: JSON.stringify(data),
-  headers: headers,
+    url: url
 };
 
-$task.fetch(myRequest).then(response => {
-    if (response.statusCode == 200){
-      // $notify("導航王測速更新", "更新成功", response.body); // Success!
-      $notify("導航王TM", "Token更新成功", response.body); // Success!
-      var obj = JSON.parse(response.body);
-      console.log(obj['output_data']['accessToken']);
-      $prefs.setValueForKey(obj['output_data']['accessToken'], 'TM_TOKEN');
-    //   $done();
-    }
-    $notify("???", "statusCode", response.statusCode); // Success!
-    // $done();
-}, reason => {
-    // reason.error
-    $notify("Refresh Key Failed", "失敗", reason.error); // Error!
-    // $done();
-});
-$notify("導航王測速更新", "Task1 Done", '');
-
-
-var TimeNow = new Date();
-var yyyy = TimeNow.toLocaleDateString().slice(0,4)
-var MM = (TimeNow.getMonth()+1<10 ? '0' : '')+(TimeNow.getMonth()+1);
-var dd = (TimeNow.getDate()<10 ? '0' : '')+TimeNow.getDate();
-let today = yyyy + "_" + MM + "_" + dd;
-console.log(today);
-const url2 = "https://download.localking.com.tw/cdnfiles/NaviKingUpdate/SpeedCameras/daily_camera_" + today + "_04.dat";
-const updateRequest = {
-    url: url2,
-    headers: headers,
-};
-$notify("導航王測速更新", "Start Task2", today);
-$task.fetch(updateRequest).then(response => {
-    if (response.statusCode == 200){
-        $notify("導航王測速更新", "更新成功", today);
-        $prefs.setValueForKey(yyyy + MM + dd, 'NK_MAP_VERSION');
-        $prefs.setValueForKey(url, 'NK_MAP_URL');
-    } else {
-        console.log('本日無更新');
-    }
-}, reason => {
-    // reason.error
-    $notify("導航王測速更新", "更新失敗", reason.error); // Error!
-});
-$done();
+if ($prefs.valueForKey("NK_MAP_VERSION") == new Date().toJSON().slice(0,10).replace(/-/g,'')){
+    body = JSON.stringify({
+        "output_data": {
+            "ClientUpdate": [{
+                "NaviKingUpdate": []
+            }],
+            "CheckNewCPInfo": 0,
+            "CameraDailyUpdate": {
+                "url": $prefs.valueForKey("NK_MAP_URL"),
+                "fileType": 0,
+                "version": parseInt($prefs.valueForKey("NK_MAP_VERSION")+"04")
+            }
+        },
+        "output_code": 1
+    });
+    $done(body);
+}else{
+    $task.fetch(myRequest).then(response => {
+        if (response.statusCode == 200){
+            body = JSON.stringify({
+                "output_data": {
+                    "ClientUpdate": [{
+                        "NaviKingUpdate": []
+                    }],
+                    "CheckNewCPInfo": 0,
+                    "CameraDailyUpdate": {
+                        "url": url,
+                        "fileType": 0,
+                        "version": parseInt(new Date().toJSON().slice(0,10).replace(/-/g,'')+"04")
+                    }
+                },
+                "output_code": 1
+            });
+            $done(body);
+        } else {
+            body = JSON.stringify({
+                "output_data": {
+                    "ClientUpdate": [{
+                        "NaviKingUpdate": []
+                    }],
+                    "CheckNewCPInfo": 0,
+                    "CameraDailyUpdate": {
+                        "url": $prefs.valueForKey("NK_MAP_URL"),
+                        "fileType": 0,
+                        "version": parseInt($prefs.valueForKey("NK_MAP_VERSION")+"04")
+                    }
+                },
+                "output_code": 1
+            });
+            $done(body);
+        }
+    }, reason => {
+        // reason.error
+        $notify("導航王測速更新", "更新失敗, 抓取離線結果", reason.error); // Error!
+        body = JSON.stringify({
+            "output_data": {
+                "ClientUpdate": [{
+                    "NaviKingUpdate": []
+                }],
+                "CheckNewCPInfo": 0,
+                "CameraDailyUpdate": {
+                    "url": $prefs.valueForKey("NK_MAP_URL"),
+                    "fileType": 0,
+                    "version": parseInt($prefs.valueForKey("NK_MAP_VERSION")+"04")
+                }
+            },
+            "output_code": 1
+        });
+        $done(body);
+    });
+}

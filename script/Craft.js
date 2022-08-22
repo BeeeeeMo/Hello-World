23457d0209e9b518e30a4085b5b368b942a41934
BeeeeeMo <BeeeeeMo0987@gmail.com>
2022-08-22 10:52:15 +0800

    craft

---
+++ script/Craft.js
@@ -0,0 +1,15 @@
+//MitM: api.craft.do
+//Regex: ^https://api\.craft\.do/auth/v2/profile
+
+var obj = JSON.parse($response.body);
+
+obj["subscription"] = {
+  "tier": "Pro",
+  "subscriptionActive": true,
+  "expirationDate": 4098510960000,
+  "subscriptionType": "yearly",
+  "rawSubscriptionType": "AppStore",
+  "productId": "com.lukilabs.lukiapp.pro.sub.yearly"
+}
+
+$done({body: JSON.stringify(obj)});
